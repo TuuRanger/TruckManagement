fd40c714f0e3ab7e662a39260f15801299faff54
TuuRanger <tuuranger@hotmail.com>
2016-04-10 23:51:31 +0700

    Up Truck Management

---
+++ CKLINE/Controllers/TTMSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CKLINE.Controllers
+{
+    public class TTMSController : Controller
+    {
+        // GET: TTMS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
